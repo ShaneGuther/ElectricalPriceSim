af89dd79873588e3c5aa8cd490f04d582d0ee0e6
ShaneGuther <46198789+ShaneGuther@users.noreply.github.com>
2020-12-21 22:09:50 -0500

    Added files

---
+++ electrical_pricing_sim/cmake-build-debug/CMakeFiles/electrical_pricing_sim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/electrical_pricing_sim.dir/main.cpp.o"
+  "electrical_pricing_sim"
+  "electrical_pricing_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/electrical_pricing_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ electrical_pricing_sim/cmake-build-debug/CMakeFiles/electrical_pricing_sim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for electrical_pricing_sim.
+# This may be replaced when dependencies are built.
